fafb6a04e0509741d49d71a89038217c466a6cf2
rrsrnc <rajeev7605@gmail.com>
2023-05-23 18:51:31 +0530

    browser router basename set to my-app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/my-app/">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
